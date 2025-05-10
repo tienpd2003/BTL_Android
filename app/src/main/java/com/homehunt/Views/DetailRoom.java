ff59aa8d5acaf8400f41481aba8f0d7f0ee3d556
Dinh Hoang Anh <110513024+Anhlearning@users.noreply.github.com>
2025-05-10 14:45:02 +0700

    Cr RoomDetail and View Room_UI

---
+++ app/src/main/java/com/homehunt/Views/DetailRoom.java
@@ -0,0 +1,37 @@
+package com.homehunt.Views;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+import com.homehunt.model.Room;
+import com.homehunt.model.UserModel;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class DetailRoom extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    public void onClick(View view){
+
+    }
+}
