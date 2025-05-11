a67c0364583dd4efbd1036d2f4e8789ec6dc1eac
Dinh Hoang Anh <110513024+Anhlearning@users.noreply.github.com>
2025-05-11 16:29:32 +0700

    Update DetailRoom.java

--- app/src/main/java/com/homehunt/Views/DetailRoom.java
+++ app/src/main/java/com/homehunt/Views/DetailRoom.java
@@ -1,37 +1,38 @@
-package com.homehunt.Views;
-import android.app.Dialog;
-import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
+package com.homehunt.views;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager.widget.ViewPager;
-import com.homehunt.model.Room;
-import com.homehunt.model.UserModel;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-
-import java.text.DecimalFormat;
-import java.util.ArrayList;
 
 public class DetailRoom extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        // TODO: Viết code xử lý khi tạo activity ở đây
+    }
+
+    private void initControl(){
+        // TODO: Khởi tạo các control (TextView, ImageView, RecyclerView, ...)
+    }
+
+    private void setInfoRoom(){
+        // TODO: Thiết lập thông tin chi tiết của phòng dựa vào đối tượng Room
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        // TODO: Xử lý khi nhấn nút quay lại trên toolbar
+        return true;
+    }
+
+    private void showImageDialog() {
+        // TODO: Hiển thị dialog hình ảnh phòng với ViewPager
+    }
+
     @Override
     public void onClick(View view){
-
+        // TODO: Xử lý các sự kiện click
     }
 }
+
